73e42a545a37edd0067c851dded204bd0509bc7f
mmbsaksd <bs.munavar@gmail.com>
2025-10-13 08:50:15 +0530

    logger file created after debugging all errors

--- research_and_analysts/logger/custom_logger.py
+++ research_and_analysts/logger/custom_logger.py
@@ -14,4 +14,33 @@ class CustomLogger:
     def get_logger(self,name=__name__):
         logger_name = os.path.basename(name)
         file_handler = logging.FileHandler(self.log_file_path)
-        
+        file_handler.setLevel(logging.INFO)
+        file_handler.setFormatter(logging.Formatter("%(message)s"))
+
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        console_handler.setFormatter(logging.Formatter("%(message)s"))
+
+        logging.basicConfig(
+            level=logging.INFO,
+            format="%(message)s",
+            handlers=[console_handler, file_handler]
+        )
+
+        structlog.configure(
+            processors=[
+                structlog.processors.TimeStamper(fmt="iso",utc=True, key="timestamp"),
+                structlog.processors.add_log_level,
+                structlog.processors.EventRenamer(to="event"),
+                structlog.processors.JSONRenderer()
+            ],
+            logger_factory=structlog.stdlib.LoggerFactory(),
+            cache_logger_on_first_use=True
+        )
+        return structlog.get_logger(logger_name)
+
+# --- Usage Example ---
+if __name__ == "__main__":
+    logger = CustomLogger().get_logger(__file__)
+    logger.info("User uploaded a file", user_id=123, filename="report.pdf")
+    logger.error("Failed to process PDF", error="File not found", user_id=123)
