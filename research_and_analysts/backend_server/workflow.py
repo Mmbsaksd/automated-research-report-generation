7abd087150ef2d53e128e619fafcc7017311fceb
mmbsaksd <bs.munavar@gmail.com>
2025-10-31 06:44:17 +0530

    bild skelton for workflow

---
+++ research_and_analysts/backend_server/workflow.py
@@ -0,0 +1,62 @@
+import os
+import sys
+from datetime import datetime
+from typing import Optional
+from langgraph.types import Send
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
+from langchain_core.messages import get_buffer_string
+from langchain_community.tools import TavilySearchResults
+
+from docx import Document
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+
+from research_and_analysts.schemas.models import(
+    Analyst,
+    Perspectives,
+    GenerateAnalystsState,
+    InterviewState,
+    ResearchGraphState
+)
+
+from research_and_analysts.utils.model_loader import ModelLoader
+
+class AutonomousReportGenerator:
+    def __init__(self):
+        pass
+
+    def create_analyst(self):
+        pass
+
+    def human_feedback(self):
+        pass
+
+    def write_report(self):
+        pass
+
+    def write_introduction(self):
+        pass
+
+    def write_conclusion(self):
+        pass
+
+    def finalize_report(self):
+        pass
+
+    def save_report(self):
+        pass
+
+    def _save_as_docs(self):
+        pass
+
+    def _save_as_pdf(self):
+        pass
+
+    def build_graph(self):
+        pass
+
+if __name__ == "__main__":
+    pass
