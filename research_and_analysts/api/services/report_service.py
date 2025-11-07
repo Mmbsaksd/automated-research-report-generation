6e5818e6897f05e68abe304e50cc57ef78e19d94
mmbsaksd <bs.munavar@gmail.com>
2025-11-07 06:41:02 +0530

    code updated

--- research_and_analysts/api/services/report_service.py
+++ research_and_analysts/api/services/report_service.py
@@ -18,10 +18,28 @@ class ReportService:
         self.logger = GLOBAL_LOGGER.bind(module='ReportService')
 
     def start_report_generation(self, topic:str, max_analyst:int):
-        pass
+        try:
+            thread_id = str(uuid.uuid4())
+            thread = {"configurable":{"thread_id":thread_id}}
+            self.logger.info("Starting report pipeline", topic=topic, thread_id=thread_id)
+            for _ in self.graph({"topic":topic, "max_analysts":max_analyst}, thread, stream_mode="values"):
+                pass
+            return {"thread_id":thread_id, "message":"Pipeline initiated successfully."}
+        except Exception as e:
+            self.logger.error("Error starting report generation", error=str(e))
+            raise ResearchAnalystException("Failed to start report generation", e)
 
     def submit_feedback(self,thread_id:str, feedback:str):
-        pass
+        try:
+            thread = {"configurable":{"thread_id":thread_id}}
+            self.graph.update_state(thread, {"human_analyst_feedback":feedback}, as_node="human_feedback")
+            self.logger.info("Feedback updated", thread_id=thread_id)
+            for _ in self.graph.stream(None, thread, stream_node="values"):
+                return {"message":"Feedback processed successfully"}
+            
+        except Exception as e:
+            self.logger.error("Error submitting feedback", error=str(e))
+            raise ResearchAnalystException("Failed to submit feedback", e)
 
     def get_report_status(self, thread_id:str):
         pass
