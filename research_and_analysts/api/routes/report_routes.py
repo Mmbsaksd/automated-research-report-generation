95f7c010964e62ca3e95888b8ad053b556c23657
mmbsaksd <bs.munavar@gmail.com>
2025-11-08 06:14:31 +0530

    db file moved to diff location

--- research_and_analysts/api/routes/report_routes.py
+++ research_and_analysts/api/routes/report_routes.py
@@ -2,3 +2,18 @@ from fastapi import APIRouter, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from sqlalchemy.orm import Session
 from research_and_analysts.api.services.report_service import ReportService
+from research_and_analysts.database.db_config import SessionLocal, User, hash_password, verify_password
+
+router = APIRouter()
+SESSION = {}
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("/", response_class=HTMLResponse)
+async def show_login(request:Request):
+    return request.app.templates.TemplateResponse("login.html",{"request":request})
