e806740ea0705db664316ba3d90f171b671f3c95
mmbsaksd <bs.munavar@gmail.com>
2025-11-08 07:17:48 +0530

    updated code route api

--- research_and_analysts/api/routes/report_routes.py
+++ research_and_analysts/api/routes/report_routes.py
@@ -17,3 +17,19 @@ def get_db():
 @router.get("/", response_class=HTMLResponse)
 async def show_login(request:Request):
     return request.app.templates.TemplateResponse("login.html",{"request":request})
+
+@router.post("/login", response_class=HTMLResponse)
+async def login(request:Request, username:str = Form(...), password:str = Form(...)):
+    db = next(get_db())
+    user = db.query(User).filter(User.username == username).first()
+
+    if user and verify_password(password, user.password):
+        session_id = f"{username}_session"
+        SESSION[session_id] = username
+        response = RedirectResponse(url="/dashboard", status_code=302)
+        response.set_cookie(key="session_id", value="session_id")
+        return response
+    return request.app.templates.TemplateResponse(
+        "login.html",
+        {"request":request, "error":"Invalid username or password"}
+    )
